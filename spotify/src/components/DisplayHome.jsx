ff7be192c30cefd753bab711b1c21d842d666cdf
Almoiz Sayyed <almoizs@heaptrace.net>
2024-06-14 11:47:31 +0530

    UI Designed Further

--- spotify/src/components/DisplayHome.jsx
+++ spotify/src/components/DisplayHome.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { albumsData, songsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl"> Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl"> Today's Biggest Hit</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={item}
+              name={item.name}
+              image={item.image}
+              desc={item.desc}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
